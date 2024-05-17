4e1a2accc4240f010b9ffc3f9f1ac47861f0260c
KoLorca <100674270+Lorinc-Vagi@users.noreply.github.com>
2024-05-17 12:40:40 +0200

    Added a bunch of stuff
    +splash js
    +css
    +bg image
    +favicon

---
+++ splash.js
@@ -0,0 +1,27 @@
+//console.log("splash.js started");
+let splashElement = document.getElementById('splash')
+
+//console.log(splashElement)
+document.getElementById('splash').innerHTML="Nyugottan. Nyugottan. Mért ne? És nyugottan.";
+
+const splashText=[
+    'Nyugottan. Nyugottan. Mért ne? És nyugottan.',
+    'Nincs süti a sötétoldalon, néztem.',
+    'MIT?',
+    'Szeretem a bundás kenyeret',
+    'MY LIFE FOR SUPER EARTH!!!'
+    
+]
+
+//console.log(splashText.length)
+
+var rndNum= Math.floor(Math.random() * splashText.length);
+document.getElementById('splash').innerHTML=splashText[rndNum];
+const date = new Date();
+let currMonth= date.getMonth()+1;
+let currDay=date.getDate();
+//console.log(currDay,currMonth)
+if (currMonth==1&&currDay==4) {
+document.getElementById('splash').innerHTML='Boldogat nekem';
+    
+}
