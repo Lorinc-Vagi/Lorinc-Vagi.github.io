56a18fb7cb29185a7b6030187cc93c95113693f8
KoLorca <100674270+Lorinc-Vagi@users.noreply.github.com>
2024-11-28 18:16:56 +0100

    svelte start

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<main>
+  <h1>Vági Lőrinc Portfoliója</h1>
+  <div class="munkaim">
+    <h2>Órai munkáim</h2>
+  </div>
+  <div class="hasznos">
+    <h2>Hasznos dolgok webfejlesztéshez</h2>
+  </div>
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/munkaim.svelte
@@ -0,0 +1,13 @@
+<script>
+    const munkaimTomb=
+    {
+        "title":"1. óra",
+        "desc":"Lorem ipsum",
+        "link":"google.com"
+    },
+    {
+        "title":"2. óra",
+        "desc":"Lorem ipsum",
+        "link":"google.com"
+    }
+</script>
